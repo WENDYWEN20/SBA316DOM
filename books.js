1802cc57b9af399eb4d81637461683197bcd2a6e
wendywen20 <wenyimei6699@gmail.com>
2024-08-21 23:53:26 -0400

    eventHandle

--- books.js
+++ books.js
@@ -1,4 +1,28 @@
-const changeRadio = document.querySelectorAll("input[type=radio]");
+//Remove Radio Button using replace
+const changeRadio =document.querySelectorAll('input[type="radio"]');
 console.log(changeRadio);
-const checkBox=document.createElement("input[type=checkbox]");
-changeRadio.replaceChild(checkBox,changeRadio)
+const checkBox=document.createElement('input');
+checkBox.type = 'checkbox';
+console.log(checkBox);
+// changeRadio.parentNode.replaceChild(checkBox, changeRadio);
+
+
+
+//Add an event Listener to the add more books read:
+
+const SubButton= document.getElementById("addBookBtn");
+const SubInput=document.getElementById("SubmitInput")
+const bookList = document.getElementById("bookList");
+console.log(SubButton)
+// The function to handle adding new comments.
+function storeSubmit() {
+    const newBook = SubInput.value;
+    if (newBook=== "") return;
+  
+    bookList.appendChild(document.createElement("li")).textContent = newBook;
+    SubInput.value = "";
+    SubInput.focus();
+  }
+  
+  // Register the event listener.
+SubButton.addEventListener("click", storeSubmit);
