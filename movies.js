13a05bfe3a95e45ca9a4278428868c7ff32a899a
wendywen20 <wenyimei6699@gmail.com>
2024-08-23 00:33:07 -0400

    DOM edited

--- movies.js
+++ movies.js
@@ -9,3 +9,13 @@ console.log(submitButton)
 submitButton.setAttribute("value", "My Favorite");
 //change button submit to "My favorite"
 
+//use style and classList for editing CSS style
+const award = document.getElementById("filmlist");
+console.log(award)
+award.style.backgroundColor = "pink";
+
+const div = document.getElementById("Movie");
+div.style.borderBlockStyle='dashed'
+
+// const list = h3.classList;
+// list.remove("myStyle");
